a2e408cdf67704d223a47e930917cc1a2668048b
Anderson Cotacachi <tu_correo@gmail.com>
2026-09-18 10:09:43 -0500

    Guias detalladas Trading: Pionex, Bitget, Pocket IA, RoboForex, Rinfinity, Weltrade, XM y Deriv

--- data/formas.ts
+++ data/formas.ts
@@ -1,9 +1,11 @@
 import { GUIAS_EXCHANGE, type Forma, type Riesgo } from "./formasExchange";
 import { GUIAS_WALLETS } from "./formasWallets";
+import { GUIAS_TRADING } from "./formasTrading";
 
 export type { Forma, Riesgo };
 
 export const GUIAS: Record<string, Forma[]> = {
   ...GUIAS_EXCHANGE,
   ...GUIAS_WALLETS,
+  ...GUIAS_TRADING,
 };
